7e546a88f02de72278492c247215b384c9c90a29
Denys Markevych <denysjse@gmail.com>
2024-07-13 16:33:05 +0300

    Create CRUD for reviews

--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -1,4 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import {BadRequestException, Injectable} from '@nestjs/common';
+import {PrismaService} from "../prisma.service";
+import {returnReviewObject} from "./return-review.object";
+import {ReviewDto} from "./dto/review.dto";
 
 @Injectable()
-export class ReviewService {}
+export class ReviewService {
+  constructor(
+    private readonly prisma: PrismaService
+  ) {}
+
+  async getAll() {
+    return this.prisma.review.findMany({
+      orderBy: {createdAt: 'desc'},
+      select: returnReviewObject
+    })
+  }
+
+  async createReview(userId: number, dto: ReviewDto, productId: number) {
+    const existProduct = await this.prisma.product.findUnique({
+      where: {id: productId}
+    })
+    if (!existProduct) throw new BadRequestException('The product with such id was not found!')
+
+    return this.prisma.review.create({
+      data: {
+        ...dto,
+        product: {
+          connect: {id: productId}
+        },
+        user: {
+          connect: {id: userId}
+        }
+      }
+    })
+  }
+
+  async getAverageValueByProductId(productId: number) {
+    const existProduct = await this.prisma.product.findUnique({
+      where: {id: productId}
+    })
+    if (!existProduct) throw new BadRequestException('The product with such id was not found!')
+
+    return this.prisma.review.aggregate({
+      where: {productId},
+      _avg: {rating: true}
+    }).then(data => data._avg)
+  }
+}
